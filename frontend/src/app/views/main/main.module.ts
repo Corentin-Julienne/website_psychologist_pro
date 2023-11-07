e1dfe719586767a44dcb058dd8f93a8f44a9d5af
Corentin JULIENNE <68602274+Corentin-Julienne@users.noreply.github.com>
2023-11-07 15:49:01 +0100

    add module for main page

--- frontend/src/app/views/main/main.module.ts
+++ frontend/src/app/views/main/main.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MainViewComponent } from "./main-view/main-view.component";
 import { MainRoutingModule } from './main-routing.module';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { HeroComponent } from './hero/hero.component';
+import { ServicesComponent } from './services/services.component';
+import { ServiceComponent } from './service/service.component';
+import { InfosComponent } from './infos/infos.component';
+import { ContactComponent } from './contact/contact.component';
+import { InfoCardsComponent } from './info-cards/info-cards.component';
+import { InfoCardComponent } from './info-card/info-card.component';
 
 @NgModule({
 	declarations: [
-		MainViewComponent
+		MainViewComponent,
+		MainHeaderComponent,
+		HeroComponent,
+		ServicesComponent,
+		ServiceComponent,
+		InfosComponent,
+		ContactComponent,
+		InfoCardsComponent,
+		InfoCardComponent
 	],
 	imports: [
 		CommonModule,
